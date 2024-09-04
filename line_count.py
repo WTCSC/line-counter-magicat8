0816d8bb77d82b500b222c444403da442929cdc7
Cameron <magicat8bill@gmail.com>
2024-09-04 13:39:08 -0600

    Changed functinality

--- line_count.py
+++ line_count.py
@@ -1,4 +1,5 @@
 def line_count():
-  """
-  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
-  """
+	file = open("file.txt", "r")
+	lines = file.readlines()
+	print(f"{len(lines)}")
+line_count()
